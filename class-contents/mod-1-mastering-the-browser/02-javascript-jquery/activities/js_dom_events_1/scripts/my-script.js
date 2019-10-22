f07ccbee0383006590a1330d66a566265601646d
Nelly Sugu <jonly03@gmail.com>
2019-10-22 05:22:05 -0300

    Add click event activity

---
+++ class-contents/mod-1-mastering-the-browser/02-javascript-jquery/activities/js_dom_events_1/scripts/my-script.js
@@ -0,0 +1,11 @@
+/*
+When the button#counter_btn is clicked, display how many times it has been clicked in the span#click_count
+*/
+
+// YOUR CODE GOES HERE
+
+/*
+When the button#reset_btn is clicked, reset the button click counter to 0
+*/
+
+// YOUR CODE GOES HERE
